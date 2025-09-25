da34df102c4ba3a847c79eaf296d87182733151f
Fredrick Kwesi Poku Dickson <fredrickdickson026@gmail.com>
2025-09-25 08:20:09 +0000

    Added Paystack keys

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,9 +1,9 @@
 class Constants {
   static const String appName = 'Invoice Generator';
   static const String paystackPublicKey =
-      'pk_test_your_key'; // Replace with your key
+      'pk_live_06f338e494438cf57b1b40e271eccb12df9f6aee'; // Replace with your key
   static const String paystackSecretKey =
-      'sk_test_your_key'; // Replace with your key
+      'sk_live_a627856c7c8272bea369b988a6c91be906d58770'; // Replace with your key
   static const String supabaseUrl = 'https://zoldbvncvfnldkgbyzzx.supabase.co';
   static const String supabaseAnonKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvbGRidm5jdmZubGRrZ2J5enp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3ODY0NTUsImV4cCI6MjA3NDM2MjQ1NX0.A9mQEKhifMsGF4QuSmdg3UhMv2USDqlCDIqpI_NyE6U';
