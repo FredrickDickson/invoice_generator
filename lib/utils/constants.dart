68a84550f7e7e8095d973a7062d670fa211cbd6d
Fredrick Kwesi Poku Dickson <fredrickdickson026@gmail.com>
2025-09-25 09:44:19 +0000

    Bug fixes

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,11 +1,9 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class Constants {
-  static const String appName = 'Invoice Generator';
-  static const String paystackPublicKey =
-      'pk_live_06f338e494438cf57b1b40e271eccb12df9f6aee'; // Replace with your key
-  static const String paystackSecretKey =
-      'sk_live_a627856c7c8272bea369b988a6c91be906d58770'; // Replace with your key
-  static const String supabaseUrl = 'https://zoldbvncvfnldkgbyzzx.supabase.co';
-  static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvbGRidm5jdmZubGRrZ2J5enp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3ODY0NTUsImV4cCI6MjA3NDM2MjQ1NX0.A9mQEKhifMsGF4QuSmdg3UhMv2USDqlCDIqpI_NyE6U';
+  static String get paystackPublicKey => dotenv.env['PAYSTACK_PUBLIC_KEY']!;
+  static String get paystackSecretKey => dotenv.env['PAYSTACK_SECRET_KEY']!;
+  static String get supabaseUrl => dotenv.env['SUPABASE_URL']!;
+  static String get supabaseAnonKey => dotenv.env['SUPABASE_ANON_KEY']!;
   static const List<String> currencies = ['USD', 'EUR', 'GBP'];
 }
