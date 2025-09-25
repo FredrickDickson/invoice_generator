364f9ed3fc8cf699ce254dc0a37e9888ef5dac23
Fredrick Kwesi Poku Dickson <fredrickdickson026@gmail.com>
2025-09-25 09:54:32 +0000

    Initial commit with updated config

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-import 'app.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Supabase.initialize(
-    url: 'https://zoldbvncvfnldkgbyzzx.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvbGRidm5jdmZubGRrZ2J5enp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3ODY0NTUsImV4cCI6MjA3NDM2MjQ1NX0.A9mQEKhifMsGF4QuSmdg3UhMv2USDqlCDIqpI_NyE6U',
+    url: Constants.supabaseUrl,
+    anonKey: Constants.supabaseAnonKey,
   );
   runApp(App());
 }
