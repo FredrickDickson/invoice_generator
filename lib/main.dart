a3d1fd64651fd6bc7e87215c23864b5a108b7e44
Fredrick Kwesi Poku Dickson <fredrickdickson026@gmail.com>
2025-09-25 08:13:37 +0000

    FIXED MAIN.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'your-supabase-url',
-    anonKey: 'your-supabase-anon-key',
+    url: 'https://zoldbvncvfnldkgbyzzx.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvbGRidm5jdmZubGRrZ2J5enp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3ODY0NTUsImV4cCI6MjA3NDM2MjQ1NX0.A9mQEKhifMsGF4QuSmdg3UhMv2USDqlCDIqpI_NyE6U',
   );
   runApp(App());
 }
